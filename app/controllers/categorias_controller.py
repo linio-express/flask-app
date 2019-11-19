180792a40af3ffa2ba8366862471a523dddb0cb6
linio-express <linio.express@gmail.com>
2019-11-18 19:26:53 -0500

    Add show method to categorias_controller

---
+++ app/controllers/categorias_controller.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template
+from flask import session, request
+from datetime import datetime
+from app.models.producto import Producto
+from app.models.usuario import Usuario
+from app.models.categoria import Categoria
+#Importar módulo con funciones de ayuda
+from app.helpers.helper import *
+import os
+
+template_dir = os.path.abspath('app/views')
+categoria = Blueprint('categoria', 'api', template_folder=template_dir)
+
+@categoria_page.route('/canasta/<int:id_categoria>', methods=['GET'])
+def show(id_categoria):
+    #Se obtiene el objeto Canasta a partir del id
+    categoria = Categoria.obtener(id_categoria)
+    #Se obtienen los productos dentro de esta categoría
+    productos = Producto.listar_productos_nombre(id_categoria = categoria.id)
+    if session.get('logged_in') == True:
+        usuario = Usuario.obtener(session['current_user_id'])
+        return render_template('productos/index.html', resultados = toJSON(productos), texto_a_buscar=texto_a_buscar, usuario=usuario.toJSON(), categoria = categoria.toJSON())
+    else:
+        session["current_user_id"] = 0
+        session["logged_in"] = False
+        return render_template('productos/index.html', logged_in = session["logged_in"], current_user_id = session["current_user_id"], resultados = toJSON(lista_productos), texto_a_buscar=texto_a_buscar, categoria = categoria.toJSON())
